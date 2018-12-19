fd4d6712f31ccba4b15dfbe4bb58fe8221c417f2
Sameer Siruguri <siruguri@gmail.com>
2018-12-18 16:03:17 -0800

    Update bundle and Ruby to 2.5.3

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 
 require 'simplecov'
 require 'rails/test_help'
-require 'mocha/mini_test'
+require 'mocha/minitest'
 require 'webmock/minitest'
 
 require 'minitest/rails/capybara'
